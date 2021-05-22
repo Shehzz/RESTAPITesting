f86f2a56793c26b893d07f7072db51376de82db0
Shehzaan Ansari <sansari@Shehzaans-Macbook-Pro.local>
2021-05-22 15:19:44 +0400

    WIP: Data verifcation + Fake name/email generator added

--- requests_test.py
+++ requests_test.py
@@ -24,16 +24,42 @@ def test_creating_a_new_user_using_POST_123(URL, auth_token):
     response = requests.post(URL, headers=auth_token, data=variables.payload)
     response_json = response.json()
 
-    #global response_id  # TODO Change this to a fixture for later use
+    # global response_id  # TODO Change this to a fixture/global for later use
 
     response_id = response_json['data']['id']
-    pytest.global_response_id = response_id
+    pytest.global_response_id = response_id  # making the response_id a GLOBAL variable
     print("\nThe userID generated is:" + str(response_id))
-    assert response_json['code'] == 201
+    assert response_json['code'] == 201, "The HTTP response code is not 201"
+    assert data_verification()
 
 
-def test_checking_for_same_existing_user_123(URL):
-    # TODO MAKE A GENERIC FUNCTION(FIXTURE) TO DO ALL THE GET ACTIONS
+@pytest.mark.parametrize(
+    "name, email, gender, status",
+    [
+        (variables.fake_name, variables.fake_email, "Male", "Active")
+    ]
+)
+def test_data_verification_method(URL, name, email, gender, status):
     response = requests.get(URL + str(pytest.global_response_id)).json()
+    assert response['data']['id'] == pytest.global_response_id
+    assert response['data']['name'] == name
+    assert response['data']['email'] == email
+    assert response['data']['gender'] == gender
+    assert response['data']['status'] == status
     assert response['code'] == 200
     print("\nThe HTTP response code received is:" + str(response['code']))
+    return True
+
+
+def data_verification():
+    response = requests.get("https://gorest.co.in/public-api/users/" + str(pytest.global_response_id)).json()
+    if response['data']['id'] == pytest.global_response_id and \
+       response['data']['name'] == variables.fake_name and \
+       response['data']['email'] == variables.fake_email and \
+       response['data']['gender'] == "Male" and \
+       response['data']['status'] == "Active":
+        print("\nData Validation success!")
+        return True
+    else:
+        print("\nData Validation failed!")
+        return False
--- variables.py
+++ variables.py
@@ -1,3 +1,9 @@
+from faker import Faker
+fake = Faker()
+
+fake_name = fake.name()
+fake_email = fake.email()
+
 # Insert VARIABLES here #
 AUTH = "b23d56310020fcd29ea38fb9426ff3d7c7fffa22af8475a53909a23aa50549d8"
-payload = {'name': 'SM 123', 'gender': 'Male', 'email': 'bb11111c@123.com', 'status': 'Active'}
+payload = {'name': fake_name, 'gender': 'Male', 'email': fake_email, 'status': 'Active'}
