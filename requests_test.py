723ae4d3d5174da032cb723bdc1c5800ce693bac
Shehzaan Ansari <sansari@Shehzaans-Macbook-Pro.local>
2021-05-22 19:08:33 +0400

    WIP: Added PUT request with new variables

--- requests_test.py
+++ requests_test.py
@@ -6,12 +6,13 @@ import variables
 def test_checking_for_200_status(URL):
     response = requests.get(URL).json()
     assert response['code'] == 200
+    #print(response)
 
 
 def test_checking_for_deleted_user(URL):
-    get_response_body = requests.get(URL + "12341111").json()
+    get_response_body = requests.get(URL + variables.deleted_user).json()
     assert get_response_body['code'] == 404
-    print("\nThe HTTP response code received is:" + str(get_response_body['code']))
+    print("\nThe user with the ID:" + variables.deleted_user + " doesn't exist & the HTTP response code received is:" + str(get_response_body['code']))
 
 
 # Verifying the body of the response headers
@@ -23,32 +24,29 @@ def test_check_content_type_equals_json(URL):
 def test_creating_a_new_user_using_POST_123(URL, auth_token):
     response = requests.post(URL, headers=auth_token, data=variables.payload)
     response_json = response.json()
-
-    # global response_id  # TODO Change this to a fixture/global for later use
-
     response_id = response_json['data']['id']
     pytest.global_response_id = response_id  # making the response_id a GLOBAL variable
     print("\nThe userID generated is:" + str(response_id))
     assert response_json['code'] == 201, "The HTTP response code is not 201"
-    assert data_verification(URL)
+    assert data_verification(URL)  # making sure all the data is valid
 
 
-@pytest.mark.parametrize(
-    "name, email, gender, status",
-    [
-        (variables.fake_name, variables.fake_email, "Male", "Active")
-    ]
-)
-def test_data_verification_method(URL, name, email, gender, status):
-    response = requests.get(URL + str(pytest.global_response_id)).json()
-    assert response['data']['id'] == pytest.global_response_id
-    assert response['data']['name'] == name
-    assert response['data']['email'] == email
-    assert response['data']['gender'] == gender
-    assert response['data']['status'] == status
-    assert response['code'] == 200
-    print("\nThe HTTP response code received is:" + str(response['code']))
-    return True
+# @pytest.mark.parametrize(
+#     "name, email, gender, status",
+#     [
+#         (variables.fake_name, variables.fake_email, "Male", "Active")
+#     ]
+# )
+# def test_data_verification_method(URL, name, email, gender, status):
+#     response = requests.get(URL + str(pytest.global_response_id)).json()
+#     assert response['data']['id'] == pytest.global_response_id
+#     assert response['data']['name'] == name
+#     assert response['data']['email'] == email
+#     assert response['data']['gender'] == gender
+#     assert response['data']['status'] == status
+#     assert response['code'] == 200
+#     print("\nThe HTTP response code received is:" + str(response['code']))
+#     return True
 
 
 def data_verification(URL):
@@ -64,3 +62,18 @@ def data_verification(URL):
     else:
         print("\nData Validation failed!")
         return False
+
+
+def test_updating_existing_user_using_PUT_123(URL, auth_token):
+    response = requests.put(URL + str(pytest.global_response_id), headers=auth_token, data=variables.updated_payload)
+    response_json = response.json()
+    assert response_json['code'] == 200, "The HTTP response code is not 200"
+    """ If the data_verification() fails here, that means the user ID has been successfully updated"""
+    if not (data_verification(URL)):
+        print("The user details have been updated successfully!")
+        print(response_json)
+        assert True
+    else:
+        print("The user details have not been updated!")
+        assert False
+
--- variables.py
+++ variables.py
@@ -4,6 +4,11 @@ fake = Faker()
 fake_name = fake.name()
 fake_email = fake.email()
 
+fake_name2 = fake.name()
+fake_email2 = fake.email()
+
 # Insert VARIABLES here #
 AUTH = "b23d56310020fcd29ea38fb9426ff3d7c7fffa22af8475a53909a23aa50549d8"
 payload = {'name': fake_name, 'gender': 'Male', 'email': fake_email, 'status': 'Active'}
+updated_payload = {'name': fake_name2, 'gender': 'Male', 'email': fake_email2, 'status': 'Active'}
+deleted_user = "123123"
