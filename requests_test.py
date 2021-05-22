c600fd1cb401c182197fc4f79fab2e118a14e43e
Shehzaan Ansari <sansari@Shehzaans-Macbook-Pro.local>
2021-05-22 20:19:16 +0400

    Removed useless code and comments

--- requests_test.py
+++ requests_test.py
@@ -6,19 +6,13 @@ import variables
 def test_checking_for_200_status(URL):
     response = requests.get(URL).json()
     assert response['code'] == 200
-    #print(response)
 
 
 def test_checking_for_deleted_user(URL):
     get_response_body = requests.get(URL + variables.deleted_user).json()
-    assert get_response_body['code'] == 404
+    assert get_response_body['code'] == 404 and \
+           get_response_body['data']['message'] == "Resource not found"
     print("\nThe user with the ID:" + variables.deleted_user + " doesn't exist & the HTTP response code received is:" + str(get_response_body['code']))
-
-
-# Verifying the body of the response headers
-def test_check_content_type_equals_json(URL):
-    response = requests.get(URL)
-    assert response.headers["Content-Type"] == "application/json; charset=utf-8"
 
 
 def test_creating_a_new_user_using_POST_123(URL, auth_token):
@@ -29,24 +23,6 @@ def test_creating_a_new_user_using_POST_123(URL, auth_token):
     print("\nThe userID generated is:" + str(response_id))
     assert response_json['code'] == 201, "The HTTP response code is not 201"
     assert data_verification(URL)  # making sure all the data is valid
-
-
-# @pytest.mark.parametrize(
-#     "name, email, gender, status",
-#     [
-#         (variables.fake_name, variables.fake_email, "Male", "Active")
-#     ]
-# )
-# def test_data_verification_method(URL, name, email, gender, status):
-#     response = requests.get(URL + str(pytest.global_response_id)).json()
-#     assert response['data']['id'] == pytest.global_response_id
-#     assert response['data']['name'] == name
-#     assert response['data']['email'] == email
-#     assert response['data']['gender'] == gender
-#     assert response['data']['status'] == status
-#     assert response['code'] == 200
-#     print("\nThe HTTP response code received is:" + str(response['code']))
-#     return True
 
 
 def data_verification(URL):
@@ -68,7 +44,9 @@ def test_updating_existing_user_using_PUT_123(URL, auth_token):
     response = requests.put(URL + str(pytest.global_response_id), headers=auth_token, data=variables.updated_payload)
     response_json = response.json()
     assert response_json['code'] == 200, "The HTTP response code is not 200"
-    """ If the data_verification() fails here, that means the user ID has been successfully updated"""
+
+    """ If the data_verification() fails here, that means the user ID has been updated successfully"""
+
     if not (data_verification(URL)):
         print("The user details have been updated successfully!")
         print(response_json)
--- variables.py
+++ variables.py
@@ -9,6 +9,7 @@ fake_email2 = fake.email()
 
 # Insert VARIABLES here #
 AUTH = "b23d56310020fcd29ea38fb9426ff3d7c7fffa22af8475a53909a23aa50549d8"
-payload = {'name': fake_name, 'gender': 'Male', 'email': fake_email, 'status': 'Active'}
-updated_payload = {'name': fake_name2, 'gender': 'Male', 'email': fake_email2, 'status': 'Active'}
+payload = {'name': fake_name, 'email': fake_email, 'gender': 'Male', 'status': 'Active'}
+updated_payload = {'name': fake_name2, 'email': fake_email2, 'gender': 'Male', 'status': 'Active'}
+faulty_payload = {'name': 'ABC ABC', 'gender': 'Male', 'status': 'Inactive'}
 deleted_user = "123123"
